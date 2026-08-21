d54df2a6ca454258379817baa320965d2a31e96a
Darkieson <154759263+Jamarblack@users.noreply.github.com>
2026-08-21 13:17:10 +0100

    Revert "Install Vercel Web Analytics"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Analytics } from '@vercel/analytics/react';
 
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -46,7 +45,6 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
       <VoiceAssistant />
-        <Analytics />
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<LandingPage />} /> 
